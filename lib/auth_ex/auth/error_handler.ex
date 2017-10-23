d76cc10edb73d742fe9b0e199ec1fb9c4f2b5361
Tyler Pachal <tylerpachalandfriends@gmail.com>
2017-10-22 22:53:03 -0400

    Fixes content type of unauthorzed response

--- lib/auth_ex/auth/error_handler.ex
+++ lib/auth_ex/auth/error_handler.ex
@@ -2,8 +2,10 @@
 defmodule AuthEx.Auth.ErrorHandler do
   import Plug.Conn
 
-  def auth_error(conn, {type, _reason}, _opts) do
+  def auth_error(conn, {type, reason}, _opts) do
     body = to_string(type)
-    send_resp(conn, 401, body)
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(401, body)
   end
 end
